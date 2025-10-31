2fffabc673efb0586becbfffc3214b001dfced0b
ANUSREE KJ <anusree.k.jinan123@gmail.com>
2025-11-01 03:12:37 +0530

    Create student table and message trigger
    AIM
    Create a trigger to print the following message when insert , delete or update operation is performed on student table for each tuple that is inserted , deleted are stored in student table along with roll number and name of students

---
+++ 11__PLSQL_TRIGGER_student.sql
@@ -0,0 +1,21 @@
+------------------TABLE CREATION------------------
+CREATE TABLE student (
+roll_no NUMBER ,
+name VARCHAR2 ,
+s1_grade NUMBER , 
+s2_grade NUMBER 
+);
+-----------------------CODE-------------------------
+SET SERVEROUTPUT ON ;
+CREATE TRIGGER message_trigger
+AFTER INSERT OR DELETE OR UPDATE ON student
+FOR EACH ROW 
+BEGIN
+ IF UPDATING THEN 
+   DBMS_OUTPUT.PUT_LINE('updated is ' || :NEW.name ) ;
+ ELSIF INSERTING THEN
+   DBMS_OUTPUT.PUT_LINE('inserting ' || :NEW.name ) ;
+ ELSIF DELETING THEN
+   DBMS_OUTPUT.PUT_LINE('deleting ' || :OLD.name) ;
+ END IF;
+END /
